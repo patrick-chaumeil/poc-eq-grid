de4faf331078809eee02c295e40c22282b9c86be
Vincent YAMAK <49241543+vyamak@users.noreply.github.com>
2020-07-10 21:19:29 +0200

    hover sur items

--- src/Item.js
+++ src/Item.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { Box, Typography } from "@material-ui/core";
 import * as Comps from "./comps";
+import { makeStyles } from "@material-ui/core/styles";
+import cx from "clsx";
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    "&:hover": {
+      backgroundColor: theme.palette.action.hover
+    }
+  }
+}));
 const DefaultComp = ({ compName }) => (
   <Typography color="error">unknown component type *{compName}*</Typography>
 );
@@ -14,10 +23,11 @@ const Item = ({
   onClick,
   isSelected
 }) => {
+  const classes = useStyles();
   const TheComp = Comps[component];
   return (
     <Box
-      className={className}
+      className={cx(className, classes.root)}
       onClick={onClick}
       border={2}
       borderColor={isSelected ? "primary.main" : "transparent"}
