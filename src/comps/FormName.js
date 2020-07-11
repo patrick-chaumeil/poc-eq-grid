423dcba9197a7fae5ec13268edc03d7dfd3cd564
Vincent YAMAK <49241543+vyamak@users.noreply.github.com>
2020-07-11 09:55:33 +0200

    fix alignement icon dans formName

--- src/comps/FormName.js
+++ src/comps/FormName.js
@@ -4,7 +4,7 @@ import { FilterRounded } from "@material-ui/icons";
 
 const FormTitle = () => (
   <Box display="flex" alignItems="center">
-    <Box mr={2}>
+    <Box display="flex" mr={2}>
       <FilterRounded />
     </Box>
     <Typography variant="h4" title="My form name" noWrap>
