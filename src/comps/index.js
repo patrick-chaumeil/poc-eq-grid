6d30283e7f1ff076223bd0d2812b7bd264af4b62
Vincent YAMAK <49241543+vyamak@users.noreply.github.com>
2020-07-11 09:47:52 +0200

    fix "unknown component type *formName*"

--- src/comps/index.js
+++ src/comps/index.js
@@ -1,7 +1,9 @@
 import Text from "./Text";
 import Date from "./Date";
 import Paper from "./Paper";
+import FormName from "./FormName";
 
 export const text = Text;
 export const date = Date;
 export const paper = Paper;
+export const formName = FormName;
